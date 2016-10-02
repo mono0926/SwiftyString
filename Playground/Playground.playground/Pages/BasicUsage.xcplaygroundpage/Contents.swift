a29c554dd54293715b2fdf450455b7748c935864
Masayuki Ono <mono0926@gmail.com>
2016-10-02 20:45:57 +0900

    Support Linux

--- Playground/Playground.playground/Pages/BasicUsage.xcplaygroundpage/Contents.swift
+++ Playground/Playground.playground/Pages/BasicUsage.xcplaygroundpage/Contents.swift
@@ -1,6 +1,12 @@
 //: Playground - noun: a place where people can play
 
+#if os(OSX)
+import Cocoa
+#elseif os(iOS)
 import UIKit
+#elseif os(Linux)
+import Glibc
+#endif
 import SwiftyStringExtension
 
 var str = "Hello, playground"
