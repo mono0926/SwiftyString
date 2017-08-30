189ada0ed3169f2f8a99af83526c3f36cd94c880
Masayuki Ono <mono0926@gmail.com>
2017-08-30 15:45:57 +0900

    Update to SwiftPM 4 manifest

--- Package.swift
+++ Package.swift
@@ -1,5 +1,10 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyString"
+    name: "SwiftyString",
+    targets: [
+        .target(name: "SwiftyString"),
+        .testTarget(name: "SwiftyStringTests", dependencies: ["SwiftyString"])
+    ]
 )
